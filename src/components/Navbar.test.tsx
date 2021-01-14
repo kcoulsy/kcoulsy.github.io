95957336f6c774b745667aba70747d04a7584173
Kristian <kristian@coulsy.co.uk>
2021-01-14 19:49:33 +0000

    Show total number of items in cart on the navbar

--- src/components/Navbar.test.tsx
+++ src/components/Navbar.test.tsx
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 
 import Navbar from './Navbar';
 import { PATH_CART, PATH_HOME } from './../constants/pageRoutes';
+import * as CartContextModule from '../contexts/cart';
 
 const setup = () => {
     return shallow(<Navbar />);
@@ -38,4 +39,55 @@ test('should contain an input for searching', () => {
     const wrapper = setup();
     const el = wrapper.find('[data-test="navbar-search"]');
     expect(el.length).toBe(1);
+});
+
+describe('cart quantity', () => {
+    test('should not show qty of items in cart on the cart link 0', () => {
+        const mockUseCartDispatchContext = jest.fn(() => []);
+        jest.spyOn(CartContextModule, 'useCartStateContext').mockImplementation(
+            mockUseCartDispatchContext,
+        );
+
+        const wrapper = setup();
+        const el = wrapper.find('[data-test="navbar-link-cart"]');
+
+        expect(el.text()).toContain('Cart');
+    });
+    test('should show qty of items in cart on the cart link if > 0', () => {
+        const mockUseCartDispatchContext = jest.fn(() => [
+            {
+                productId: 1,
+                quantity: 1,
+            },
+        ]);
+        jest.spyOn(CartContextModule, 'useCartStateContext').mockImplementation(
+            mockUseCartDispatchContext,
+        );
+
+        const wrapper = setup();
+        const el = wrapper.find('[data-test="navbar-link-cart"]');
+
+        expect(el.text()).toContain('1)');
+    });
+
+    test('should contain number of all of quantities of each item added together', () => {
+        const mockUseCartDispatchContext = jest.fn(() => [
+            {
+                productId: 1,
+                quantity: 10,
+            },
+            {
+                productId: 2,
+                quantity: 3,
+            },
+        ]);
+        jest.spyOn(CartContextModule, 'useCartStateContext').mockImplementation(
+            mockUseCartDispatchContext,
+        );
+
+        const wrapper = setup();
+        const el = wrapper.find('[data-test="navbar-link-cart"]');
+
+        expect(el.text()).toContain('13');
+    });
 });
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useCartStateContext } from '../contexts/cart';
 import { PATH_CART, PATH_HOME } from './../constants/pageRoutes';
 
 const Navbar: React.FC = () => {
+    const cartItems = useCartStateContext();
+    const totalQty = cartItems.reduce((acc, curr) => {
+        acc += curr.quantity;
+        return acc;
+    }, 0);
+
     return (
         <nav data-test="component-navbar">
             <div>
@@ -14,7 +21,7 @@ const Navbar: React.FC = () => {
                     data-test="navbar-search"
                 />
                 <Link to={PATH_CART} data-test="navbar-link-cart">
-                    Cart
+                    Cart {totalQty > 0 && `(${totalQty})`}
                 </Link>
             </div>
         </nav>
