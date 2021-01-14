707cbb384108cdcc6fbf3b361a99d9b69ed8a555
Kristian <kristian@coulsy.co.uk>
2021-01-14 19:19:39 +0000

    dispatch cart updates when clicking qty buttons

--- src/components/CartProduct.test.tsx
+++ src/components/CartProduct.test.tsx
@@ -5,7 +5,7 @@ import * as CartContextModule from '../contexts/cart';
 
 const testProduct = {
     productId: 1,
-    quantity: 1,
+    quantity: 5,
 };
 
 const setup = () => {
@@ -25,11 +25,98 @@ test('should render without error', () => {
 
 test('should display product name', () => {});
 
-describe('product quanitity', () => {
-    test('should display product quantity', () => {});
-    test('should display product increment button', () => {});
-    test('should display product decrement button', () => {});
-    test('should dispatch quanity update on change', () => {});
+test('should fetch product details', () => {});
+
+describe('product quantity', () => {
+    test('should display product quantity', () => {
+        const wrapper = setup();
+        const el = wrapper.find('[data-test="cartproduct-quantity"]');
+        expect(el.text()).toBe(testProduct.quantity.toString());
+    });
+
+    test('should display product increment button', () => {
+        const wrapper = setup();
+        const el = wrapper.find('[data-test="cartproduct-qty-inc"]');
+        expect(el.length).toBe(1);
+    });
+
+    test('should display product decrement button', () => {
+        const wrapper = setup();
+        const el = wrapper.find('[data-test="cartproduct-qty-dec"]');
+        expect(el.length).toBe(1);
+    });
+
+    test('should dispatch quanity update on incrementing', () => {
+        const mockUseCartDispatchContext = jest.fn();
+        jest.spyOn(
+            CartContextModule,
+            'useCartDispatchContext',
+        ).mockImplementation(() => mockUseCartDispatchContext);
+
+        const wrapper = setup();
+        const el = wrapper.find('[data-test="cartproduct-qty-inc"]');
+        el.simulate('click', { preventDefault() {} });
+
+        expect(mockUseCartDispatchContext).toHaveBeenCalledTimes(1);
+        expect(mockUseCartDispatchContext).toHaveBeenCalledWith({
+            type: CartContextModule.CartActionType.UpdateQuantity,
+            payload: {
+                productId: testProduct.productId,
+                quantity: testProduct.quantity + 1,
+            },
+        });
+    });
+
+    test('should dispatch quanity update on decrementing', () => {
+        // TODO stop repeating these, stick it at top of describe and clear afterEach
+        const mockUseCartDispatchContext = jest.fn();
+        jest.spyOn(
+            CartContextModule,
+            'useCartDispatchContext',
+        ).mockImplementation(() => mockUseCartDispatchContext);
+
+        const wrapper = setup();
+        const el = wrapper.find('[data-test="cartproduct-qty-dec"]');
+        el.simulate('click', { preventDefault() {} });
+
+        expect(mockUseCartDispatchContext).toHaveBeenCalledTimes(1);
+        expect(mockUseCartDispatchContext).toHaveBeenCalledWith({
+            type: CartContextModule.CartActionType.UpdateQuantity,
+            payload: {
+                productId: testProduct.productId,
+                quantity: testProduct.quantity - 1,
+            },
+        });
+    });
+
+    test('should dispatch remove on decrementing when quantity is 1', () => {
+        const testProductWithSingleQty = {
+            productId: 1,
+            quantity: 1,
+        };
+        const mockUseCartDispatchContext = jest.fn();
+        jest.spyOn(
+            CartContextModule,
+            'useCartDispatchContext',
+        ).mockImplementation(() => mockUseCartDispatchContext);
+
+        const wrapper = shallow(
+            <CartProduct
+                productId={testProductWithSingleQty.productId}
+                quantity={testProductWithSingleQty.quantity}
+            />,
+        );
+        const el = wrapper.find('[data-test="cartproduct-qty-dec"]');
+        el.simulate('click', { preventDefault() {} });
+
+        expect(mockUseCartDispatchContext).toHaveBeenCalledTimes(1);
+        expect(mockUseCartDispatchContext).toHaveBeenCalledWith({
+            type: CartContextModule.CartActionType.RemoveFromCart,
+            payload: {
+                productId: testProduct.productId,
+            },
+        });
+    });
 });
 
 describe('remove product button', () => {
--- src/components/CartProduct.tsx
+++ src/components/CartProduct.tsx
@@ -18,9 +18,53 @@ const CartProduct: React.FC<CartProductProps> = ({ productId, quantity }) => {
         });
     };
 
+    const handleQtyIncrement = (ev: React.MouseEvent<HTMLButtonElement>) => {
+        dispatch({
+            type: CartActionType.UpdateQuantity,
+            payload: {
+                productId,
+                quantity: quantity + 1,
+            },
+        });
+    };
+    const handleQtyDecrement = (ev: React.MouseEvent<HTMLButtonElement>) => {
+        if (quantity === 1) {
+            dispatch({
+                type: CartActionType.RemoveFromCart,
+                payload: {
+                    productId,
+                },
+            });
+            return;
+        }
+        dispatch({
+            type: CartActionType.UpdateQuantity,
+            payload: {
+                productId,
+                quantity: quantity - 1,
+            },
+        });
+    };
+
     return (
         <div data-test="component-cartproduct">
-            Item: {productId} - Qty: {quantity}
+            Item: {productId}
+            <div>
+                Qty:
+                <button
+                    data-test="cartproduct-qty-inc"
+                    onClick={handleQtyIncrement}
+                >
+                    +
+                </button>
+                <span data-test="cartproduct-quantity">{quantity}</span>
+                <button
+                    data-test="cartproduct-qty-dec"
+                    onClick={handleQtyDecrement}
+                >
+                    -
+                </button>
+            </div>
             <button
                 onClick={handleRemoveItem}
                 data-test="cartproduct-removebutton"
