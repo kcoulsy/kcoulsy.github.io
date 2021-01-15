78c9923550cb9d3ae96ef68fbb312665b1cf9c0c
Kristian <kristian@coulsy.co.uk>
2021-01-15 11:54:01 +0000

    added styling to navbar and base icon compnent

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCartStateContext } from '../contexts/cart';
 import { PATH_CART, PATH_HOME } from './../constants/pageRoutes';
+import Icon, { IconType } from './Icon';
 
 const Navbar: React.FC = () => {
     const { totalQuantity } = useCartStateContext();
 
     return (
-        <nav data-test="component-navbar">
-            <div>
-                <Link to={PATH_HOME} data-test="navbar-link-home">
-                    Home
+        <header data-test="component-navbar" className="bg-gray-100">
+            <nav className="container mx-auto flex items-center justify-between w-full text-gray-800 p-4">
+                <Link
+                    to={PATH_HOME}
+                    data-test="navbar-link-home"
+                    className="font-semibold text-3xl tracking-tight hover:text-gray-600"
+                >
+                    PLT
                 </Link>
-                <Link to={PATH_CART} data-test="navbar-link-cart">
-                    Cart {totalQuantity > 0 && `(${totalQuantity})`}
-                </Link>
-            </div>
-        </nav>
+                <div className="md:items-center md:w-auto flex">
+                    <Link
+                        to={PATH_CART}
+                        data-test="navbar-link-cart"
+                        className="block text-gray-800 mr-4 font-bold hover:text-gray-600"
+                    >
+                        <Icon icon={IconType.Basket} styles="w-8 h-8 mx-auto" />
+                        Cart {totalQuantity > 0 && `(${totalQuantity})`}
+                    </Link>
+                </div>
+            </nav>
+        </header>
     );
 };
 
